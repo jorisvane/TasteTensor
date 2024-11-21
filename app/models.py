422f08d4fe3ac03fa8fc621959360e30cd2391cb
jorisvane <jorisvane@gmail.com>
2024-11-21 15:09:13 +0100

    Initial fast api setup

--- app/models.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-class Recipe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    cleaned_ingredients = db.Column(db.ARRAY(db.String), nullable=False)
-    ingredients_vector = db.Column(db.ARRAY(db.Float), nullable=False)
--- run.py
+++
@@ -1,7 +0,0 @@
-# This is the entry point that initializes the flask app from __init__.py
-
-from app import app
-
-if __name__ == "__main__":
-    # run flask app that is initialized (debug mode)
-    app.run(debug=True)
