ae809bf3e0f7c84c6ec0737ac0da4ca5923f6a4f
jorisvane <jorisvane@gmail.com>
2024-12-16 18:40:43 +0100

    recipe embedding to supabase

--- scripts/embed_recipes.py
+++ scripts/embed_recipes.py
@@ -1,12 +1,33 @@
-import json
 from sentence_transformers import SentenceTransformer
-
 from supabase import create_client
 from app.config import settings
 
 supabase = create_client(settings.supabase_url, settings.supabase_key)
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-# embeddings = model.encode(sentences)
 
-data = supabase.table('recipe_data').select('id, Cleaned_ingredients').execute()
+def process_row(row):
+    cleaned_ingredients = row['Cleaned_Ingredients']
+    embedding = model.encode(cleaned_ingredients)
+    return embedding
+
+def fetch_batch_from_supabase(last_processed_id, batch_size):
+    response = supabase.table('recipe_data').select('id, Cleaned_Ingredients').is_('Embedding', None).gt('id', last_processed_id).order('id').limit(batch_size).execute()
+    rows = response.data
+    return rows
+
+def proces_database(batch_size=500):
+    last_processed_id = 0
+    while True:
+        rows = fetch_batch_from_supabase(last_processed_id, batch_size)
+        if not rows:
+            print('All rows from database are processed')
+            break
+        for row in rows:
+            embedding = process_row(row)
+            embedding = embedding.tolist()
+            supabase.table('recipe_data').update({"Embedding": embedding}).eq("id", row['id']).execute()
+            last_processed_id = row['id']
+        print(f"Processed up to row ID {last_processed_id}")
+
+proces_database()
