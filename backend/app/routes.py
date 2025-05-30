398c53fa9a8f05c0090e1c60d663631a287923a4
Joris <jorisvane@gmail.com>
2025-05-30 16:02:19 +0200

    feat(init) start backend

--- backend/app/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from backend.app.database import supabase
-
-router = APIRouter()
-
-@router.get("/first-row")
-async def get_first_row():
-    data = supabase.table("recipe_data").select("*").limit(1).execute()
-    return data.data[0]['Title'] if data.data else {"message": "No data found"}
