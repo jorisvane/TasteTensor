9d22283e815b276cbe85e73dc001b9058974ef63
Joris van Eekeren <jorisvane@gmail.com>
2025-07-12 00:42:21 +0200

    feat(searchbar) enter on search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 import { FaSearch } from 'react-icons/fa';
 
 export default function SearchBar({ onSearch }) {
+
+  const [input, setInput] = useState('');
+
   return (
     <div className="search-bar">
       <FaSearch className="search-icon" />
       <input
         type="text"
-        placeholder="Type to search..."
-        onChange={(e) => onSearch(e.target.value)}
+        placeholder="Type to search and enter..."
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            onSearch(input);
+          }
+        }}
       />
     </div>
   );
